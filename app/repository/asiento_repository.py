d7cdad8ef270552b37471e6e595d20dc49df7bbc
Juan Huerfanoxxx <juanfelipehrua@gmail.com>
2025-11-28 00:46:43 -0500

    test: Pruebas automatizadas de Asientos exitosas (US-007, US-008)

--- app/repository/asiento_repository.py
+++ app/repository/asiento_repository.py
@@ -1,36 +1,34 @@
-# app/repository/asiento_repository.py
 from sqlalchemy.orm import Session
+from sqlalchemy import func
+from typing import List
 from app.domain.asiento_model import Asiento
-from app.schemas.asiento_schema import AsientoCreate, AsientoUpdate
-
+from app.schemas.asiento_schema import AsientoUpdate
 
 class AsientoRepository:
+    @staticmethod
+    def create_bulk(db: Session, asientos: List[Asiento]):
+        db.add_all(asientos)
+        db.commit()
+        return len(asientos)
 
     @staticmethod
-    def create(db: Session, data: AsientoCreate):
-        asiento = Asiento(**data.dict())
-        db.add(asiento)
-        db.commit()
-        db.refresh(asiento)
+    def get_by_evento(db: Session, e_id: int):
+        return db.query(Asiento).filter(Asiento.evento_id == e_id).order_by(Asiento.seccion, Asiento.fila, Asiento.numero).all()
+
+    @staticmethod
+    def count_by_evento(db: Session, e_id: int):
+        return db.query(func.count(Asiento.asiento_id)).filter(Asiento.evento_id == e_id).scalar()
+
+    @staticmethod
+    def get_by_id(db: Session, id: int):
+        return db.query(Asiento).filter(Asiento.asiento_id == id).first()
+
+    @staticmethod
+    def update(db: Session, asiento: Asiento, data: AsientoUpdate):
+        for k, v in data.model_dump(exclude_unset=True).items(): setattr(asiento, k, v)
+        db.commit(); db.refresh(asiento)
         return asiento
 
     @staticmethod
-    def get_by_id(db: Session, asiento_id: int):
-        return db.query(Asiento).filter(Asiento.asiento_id == asiento_id).first()
-
-    @staticmethod
-    def list_by_evento(db: Session, evento_id: int):
-        return db.query(Asiento).filter(Asiento.evento_id == evento_id).all()
-
-    @staticmethod
-    def update(db: Session, asiento, data: AsientoUpdate):
-        for field, value in data.dict(exclude_unset=True).items():
-            setattr(asiento, field, value)
-        db.commit()
-        db.refresh(asiento)
-        return asiento
-
-    @staticmethod
-    def delete(db: Session, asiento):
-        db.delete(asiento)
-        db.commit()
+    def delete(db: Session, asiento: Asiento):
+        db.delete(asiento); db.commit()
